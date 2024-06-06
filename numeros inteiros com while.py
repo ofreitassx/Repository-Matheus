77df7e1f6e74c548e0db8e225339259c58bc0baf
Lucas de Paula Freitas <166523130+ofreitassx@users.noreply.github.com>
2024-06-06 09:20:36 -0300

    Add files via upload

---
+++ numeros inteiros com while.py
@@ -0,0 +1,9 @@
+soma = 0
+contador = 0
+if 1 >= 0:
+     while contador < 10:
+         numero = int(input("DIGITE UM NUMERO INTEIRO: "))
+         soma += numero
+         print (soma)
+else:
+     print ("O NUMERO NÃO É INTEIRO")
