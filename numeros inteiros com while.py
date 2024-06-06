3a294bdeab15feeb57a745213da016cce2db3562
Lucas de Paula Freitas <166523130+ofreitassx@users.noreply.github.com>
2024-06-06 09:38:08 -0300

    Add files via upload

---
+++ numeros inteiros com while.py
@@ -0,0 +1,10 @@
+soma = 0
+contador = 0
+while contador < 10:
+      numero = int(input("DIGITE UM NUMERO INTEIRO: "))
+      soma += numero
+      contador += 1
+if numero > 0 or numero == 0:
+      print (soma)
+if numero < 0:
+      print ("O NUMERO É NEGATIVO")
