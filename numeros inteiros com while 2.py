18e16e1ac207cedda83c5c35ecc43d9a904dcd85
Lucas de Paula Freitas <166523130+ofreitassx@users.noreply.github.com>
2024-06-06 09:46:14 -0300

    Delete numeros inteiros com while 2.py

--- numeros inteiros com while 2.py
+++
@@ -1,9 +0,0 @@
-soma = 0
-print ("DIGITE 0 PARA FECHAR")
-while True:
-    n1 = int(input("DIGITE UM NUMERO POSITIVO: "))
-    if n1 == 0:
-        break
-    soma += n1
-
-print("A soma de todos os números digitados é:", soma)
