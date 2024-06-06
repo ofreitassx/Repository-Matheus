420246bfd0cf5010e1c02c444d7aef313f03d9f8
Lucas de Paula Freitas <166523130+ofreitassx@users.noreply.github.com>
2024-06-06 08:14:23 -0300

    Add files via upload

---
+++ while 1.py
@@ -0,0 +1,7 @@
+soma = 0
+contador = 0
+while contador < 10:
+    numero = int(input("DIGITE UM NUMERO INTEIRO: "))
+    soma += numero
+    contador += 1
+print ("A SOMA DE 10 NUMEROS INTEIROS É: ", soma)
