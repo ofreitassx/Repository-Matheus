e1216c8814b8713389ef45803fa3047452eda315
Lucas de Paula Freitas <166523130+ofreitassx@users.noreply.github.com>
2024-06-11 10:11:53 -0300

    Add files via upload

---
+++ lista de alunos.py
@@ -0,0 +1,13 @@
+
+lista = []
+lista.append ('lucas')
+lista.append ('messi')
+lista.append ('gabigol')
+lista.append ('arrascaeta')
+lista.append ('de la cruz')
+lista.append ('pedro')
+lista.append ('rossi')
+lista.append ('leo pereira')
+lista.append ('varela')
+lista.append ('pulgar')
+print (lista)
